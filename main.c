622a50967336f94de5f5fa6596e70682621e1000
pontus.rosenquist@gmail.com <pontus.rosenquist@gmail.com>
2025-03-18 17:49:56 +0100

    commit 2

--- main.c
+++ main.c
@@ -3,11 +3,18 @@
 
 int main()
 {
-    //  The Line below must *NOT* be changed or test will fail!
+    // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
    
     // Your code goes here
+    // Define variables
+    int dice_rolls[7]; // I want to use rand() % 6 + 1 and store in slot 1 2 3 4 5 6, but leave 0 empty in this array of dice_rolls
+    int total_sum = 0;
+    float median_value = 0; // %.1f Remember to use one decimal
+
+
+
 
     return 0;
 }
